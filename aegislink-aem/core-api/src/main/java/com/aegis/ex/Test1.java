1736bfced0d2edc8074e9f1f0512ed5843945f3e
sureshponnada <sureshkponnada@gmail.com>
2019-07-17 07:17:36 -0400

    Added new method getTest1 to Test1.java

--- aegislink-aem/core-api/src/main/java/com/aegis/ex/Test1.java
+++ aegislink-aem/core-api/src/main/java/com/aegis/ex/Test1.java
@@ -5,5 +5,8 @@ public class Test1 {
 	public String getMessag(String param) {
 		return "Test1 "+ param;
 	}
+	public String getTest1(String param) {
+		return "Added this for Test_branch_1";
+	}
 
 }
